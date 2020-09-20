912e64d5732782eacbc2e1eba92825978300c8b7
DrBlokmeister <57352628+DrBlokmeister@users.noreply.github.com>
2020-09-20 21:31:50 +0200

    Revert "Try to remove active changes from vscode plugin"
    This reverts commit 8dbac32cd560d2c50f6806d24f3a12c9761a32b0.

--- custom_components/breaking_changes/const.py
+++ custom_components/breaking_changes/const.py
@@ -2,8 +2,9 @@
 # Base component constants
 DOMAIN = "breaking_changes"
 DOMAIN_DATA = "{}_data".format(DOMAIN)
-VERSION = "0.4.1"
+VERSION = "0.3.6"
 PLATFORMS = ["sensor"]
+REQUIRED_FILES = ["const.py", "sensor.py"]
 ISSUE_URL = "https://github.com/custom-components/breaking_changes/issues"
 
 STARTUP = """
@@ -17,14 +18,13 @@ If you have any issues with this you need to open an issue here:
 """
 
 # Operational
-URL = "https://hachanges.entrypoint.xyz/{0}-{1}/json"
+URL = "https://hachanges.halfdecent.io/{}/json"
 
 # Icons
 ICON = "mdi:package-up"
 
 # Configuration
 CONF_NAME = "name"
-CONF_SCAN_INTERVAL = "scan_interval"
 
 # Defaults
 DEFAULT_NAME = "Potential breaking changes"
