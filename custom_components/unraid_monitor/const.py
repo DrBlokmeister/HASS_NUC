cd941e41c527d11ad24ce1c9e723fba1eedffe9d
Blokmeister <sander.blok@gmail.com>
2025-04-22 10:01:47 +0200

    ❌Remove my own unraid custom_component

--- custom_components/unraid_monitor/const.py
+++
@@ -1,9 +0,0 @@
-DOMAIN = "unraid_monitor"
-
-CONF_KEY = "key"
-CONF_POLL_INTERVAL = "poll_interval"
-
-DEFAULT_HOST = "tower.local"
-DEFAULT_USERNAME = "root"
-DEFAULT_PORT = 22
-DEFAULT_POLL_INTERVAL = 30
